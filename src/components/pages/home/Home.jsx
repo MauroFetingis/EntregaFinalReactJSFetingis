89e7f2e7accfb2d4f42bb553dd10c388b0d9e51c
Mauro Fetingis <maurofetingis17@gmail.com>
2023-08-06 23:48:26 -0300

    commit 2

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MainCarousel from '../../headerCarousel/MainCarousel'
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
